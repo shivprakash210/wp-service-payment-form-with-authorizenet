ae35135d1abf4d2ad644b6f35edac73b2a6d2113
jimcutler <63164721+jimcutler@users.noreply.github.com>
2020-10-18 12:12:25 -0700

    Optional fields & maxlength for authorize.net
    Added optional fields for company name & description; verify optional fields are in postData; added maxlength property option; fixed error from extra comma.

--- admin/settings-document.php
+++ admin/settings-document.php
@@ -4,6 +4,7 @@
 <p>You can add or modify any fields using filter "wpspf_frontend_form_fields" </p>
 <p>You can modify form post data using filter "wpspf_payment_form_post_data" </p>
 <h4>Form fields name</h4>
+    <p>The following fields are optional, and can be added to your Service Payment form as inputs with these names, or added to the post data using the filter "wpspf_payment_form_post_data".</p>
 <table class="wp-list-table widefat fixed striped">
 	<tr>
 		<th>Field Name</th>
@@ -13,14 +14,40 @@
 		<th>wpspf_x_cust_id</th>
 		<td>
 			<p>Optional</p>
-			<p><b>Value:</b> The merchant assigned customer ID</p>
+			<p><b>Value:</b> The merchant assigned customer ID.</p>
 			<p><b>Default value:</b>  mt_rand() generate random unique customer ID. If you want to accept customer ID from the customer then add field which name must be "wpspf_x_cust_id".</p>
-			<p><b>Format:</b> 20-character maximum (no symbols)</p>
+			<p><b>Format:</b> 20-character maximum (no symbols).</p>
 			<p><b>Notes:</b> The unique identifier that represents the customer associated with
 the transaction.</p>
 			<p>The customer ID must be created dynamically on the merchant server or
 provided per transaction. The payment gateway does not perform this
 function.</p>
+		</td>
+	</tr>
+	<tr>
+		<th>company_name</th>
+		<td>
+			<p>Optional</p>
+			<p><b>Value:</b> The company associated with the customer’s billing address.</p>
+			<p><b>Format:</b> 50-character maximum (no symbols).</p>
+		</td>
+	</tr>
+	<tr>
+		<th>invoice_number</th>
+		<td>
+			<p>Optional</p>
+			<p><b>Value:</b> The merchant-assigned invoice number for the transaction.</p>
+			<p><b>Format:</b> 20-character maximum (no symbols).</p>
+			<p><b>Notes:</b> The invoice number must be created dynamically on the merchant server or provided per transaction. The payment gateway does not perform this function.</p>
+		</td>
+	</tr>
+	<tr>
+		<th>description</th>
+		<td>
+			<p>Optional</p>
+			<p><b>Value:</b> The transaction description.</p>
+			<p><b>Format:</b> 255-character maximum (no symbols).</p>
+			<p>The description must be created dynamically on the merchant server or provided per transaction. The payment gateway does not perform this function.</p>
 		</td>
 	</tr>
 </table>
